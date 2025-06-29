c3cbb0882181f26b7f38278fd5d8c1e5b4ddc680
jeffin-406 <jeffinphilip460@gmail.com>
2025-06-29 22:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
+<script>
+  const heading = document.getElementById("name-heading");
+  const panel = document.getElementById("intro-panel");
+
+  heading.addEventListener("click", () => {
+    panel.classList.toggle("show");
+  });
+</script>
